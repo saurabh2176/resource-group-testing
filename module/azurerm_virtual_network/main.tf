df06cdba3d8209f944374d14b095e1f9bebab2b0
Saurabh Kumar Singh <saurabhsingh2176@gmail.com>
2026-08-16 11:49:06 +0530

    formating changes

--- module/azurerm_virtual_network/main.tf
+++ module/azurerm_virtual_network/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet-prod" {
-    for_each = var.vnet1
+  for_each            = var.vnet1
   name                = each.value.name
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
